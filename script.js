3e829d53d637ee76f8dee3282249427435a218b2
e8olution <evzen.fc@gmail.com>
2025-10-01 18:58:07 +0300

    done 4ex. from 1hw.

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 
-let creatorJavaScript=(alert('Brendan Eich'));
+let a=10;
+let b=2;
+sum=a+b;
+difference=a-b; 
+multiplication=a*b;  
+remainderdivision=a%b;
+alert (`        сумма=${sum},
+        разность=${difference},
+        произведение=${multiplication},
+        частное (результат деления)=${remainderdivision}`)
