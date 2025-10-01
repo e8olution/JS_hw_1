4544e9342aa67903724052bd92b9e560956037cc
e8olution <evzen.fc@gmail.com>
2025-10-01 19:01:38 +0300

    done 5ex. from 1hw.

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-let a=10;
-let b=2;
-sum=a+b;
-difference=a-b; 
-multiplication=a*b;  
-remainderdivision=a%b;
-alert (`        сумма=${sum},
-        разность=${difference},
-        произведение=${multiplication},
-        частное (результат деления)=${remainderdivision}`)
+
+let result =2;
+alert(result**5);
