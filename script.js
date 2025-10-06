a84ed5bb4bed06e956792af842d001eb17f9cb74
e8olution <evzen.fc@gmail.com>
2025-10-06 18:08:09 +0300

    add animation on top and scroll from button to about games

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
-
+document.getElementById("scrollTolets-go").addEventListener("click", function() {
+  document.getElementById("lets-go").scrollIntoView({ behavior: "smooth" });
+});
 //let name=prompt('Введите ваше имя');
 
 //alert(`Привет, ${name}`);
