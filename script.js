05174255abb560a6ec4280cc7cff21f635956fa8
e8olution <evzen.fc@gmail.com>
2025-10-07 11:44:43 +0300

    minor changes2

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-
+//Анимация для Главной Первый блок
 document.getElementById("scrollTolets-go").addEventListener("click", function() {
   document.getElementById("lets-go").scrollIntoView({ behavior: "smooth" });
 });
+
+
+
+
 //let name=prompt('Введите ваше имя');
 
 //alert(`Привет, ${name}`);
+
+let password='пароль';
+let answer=prompt('Введите пароль');
+if (answer===password) {
+  console.log ("Пароль введен верно");
+}
+else {
+  console.log("Пароль введен неверно");
+} 
