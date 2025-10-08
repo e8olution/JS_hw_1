aa9aa5fa1054b02a05097d1b15735877bfed855e
e8olution <evzen.fc@gmail.com>
2025-10-08 16:54:40 +0300

    add ex.1 hw (Циклы)

--- script.js
+++ script.js
@@ -4,44 +4,13 @@ document.getElementById("scrollTolets-go").addEventListener("click", function() 
 });
 
 
+let k=100;
+let iterations=0;
 
-
-//let name=prompt('Введите ваше имя');
-
-//alert(`Привет, ${name}`);
-
-//var monthNumber=prompt("Введите номер месяца")
-//monthNumber = parseInt(monthNumber);
-
-//switch (true) {
-//  case (monthNumber === 12 || monthNumber === 2 || monthNumber === 1):
-//    console.log('Зима');
-//    break;
-//  case (monthNumber>=3 && monthNumber<=5 ):
-//    console.log('Весна');
-//    break; 
-//  case (monthNumber>=6 && monthNumber<=8 ):
-//    console.log('Лето');
-//    break;     
-//  case (monthNumber>=9 && monthNumber<=11 ):
-//    console.log('Осень');
-//    break;    
-//  default:
-//    console.log('такого месяца не существует');
-//}
-
-//let obj={
-//  "Коля" : '200',
-//  "Вася" : '300',
-//  "Петя" : '400'
-//}
-//for (let key in obj) {
-//  console.log(`имя сотрудника ${key} — зарплата ${obj[key]} долларов`);
-//}
-
-let num = 0;
-for (let n = 1000; n > 50; n /= 2) {
-  console.log(n);
-  num++;
+while (k>=0) {
+  k=k-7;
+  iterations++;
 }
-console.log(num);
+
+console.log(k);
+console.log(iterations);
