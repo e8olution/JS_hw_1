c594c2713b053a61b00f62a7a22e0420111be383
e8olution <evzen.fc@gmail.com>
2025-10-08 17:24:35 +0300

    all from hw(Циклы) done

--- script.js
+++ script.js
@@ -3,12 +3,3 @@ document.getElementById("scrollTolets-go").addEventListener("click", function() 
   document.getElementById("lets-go").scrollIntoView({ behavior: "smooth" });
 });
 
-let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-let min= parseInt(numbers[0]);
-for (i=0; i<numbers.length; i++) {
-  let currentNumber = parseInt(numbers[i]);
-  if (currentNumber < min) {
-    min=currentNumber;
-  }
-}
-console.log(`Минимальное число:`,min);
