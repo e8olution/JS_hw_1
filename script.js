eb2c6554682bf1d54c5197cf8bacc4645a68b94d
e8olution <evzen.fc@gmail.com>
2025-10-14 15:18:43 +0300

    add js for game1, hw ( Функции)

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.getElementById("scrollTolets-go").addEventListener("click", function() 
   document.getElementById("lets-go").scrollIntoView({ behavior: "smooth" });
 });
 
+// Игра «Угадай число» запускается по нажатию на кнопку
+document.querySelector('#game1 .mg-item__info__button').addEventListener('click', function () {
+  const getNumber = Math.floor(Math.random() * 100) + 1;
+  let userAnswer;
+
+  while ((userAnswer = +prompt('Введите число от 1 до 100')) !== getNumber) {
+    alert(userAnswer > getNumber
+      ? 'Загаданное число меньше, попробуйте еще раз'
+      : 'Загаданное число больше, попробуйте еще раз'
+    );
+  }
+  alert('Поздравляем! Вы угадали число ');
+});
